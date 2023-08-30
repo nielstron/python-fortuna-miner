af59d26b12b6e63e9323117580f769e43118bc47
Niels Mündler <n.muendler@posteo.de>
2023-08-30 11:34:54 +0200

    Loop properly

--- src/off_chain/mine.py
+++ src/off_chain/mine.py
@@ -160,123 +160,131 @@ def main(preview: bool, mine: bool):
             print(f"Cexplorer: https://cexplorer.io/tx/{signed_tx.id}")
         return
 
-    validator_out_ref = None
-    print("Mining...")
-    last_time = 0
-    nonce = int.from_bytes(random.randbytes(16), "big")
-    i = 0
     while True:
-        i += 1
-        if time.time() - last_time > 10:
-            print(f"New block not found in 10 seconds, updating state after {i} tries")
+        try:
+            validator_out_ref = None
+            print("Mining...")
+            last_time = 0
+            nonce = int.from_bytes(random.randbytes(16), "big")
             i = 0
-            last_time = time.time()
-            validator_out_ref_new = [
-                u
-                for u in context.utxos(script_address)
-                if u.output.amount.multi_asset.get(script_hash).get(
-                    AssetName(b"lord tuna")
+            while True:
+                i += 1
+                if time.time() - last_time > 10:
+                    print(f"New block not found in 10 seconds, updating state after {i} tries")
+                    i = 0
+                    last_time = time.time()
+                    validator_out_ref_new = [
+                        u
+                        for u in context.utxos(script_address)
+                        if u.output.amount.multi_asset.get(script_hash).get(
+                            AssetName(b"lord tuna")
+                        )
+                    ][0]
+
+                    if validator_out_ref_new != validator_out_ref:
+                        validator_out_ref = [
+                            u
+                            for u in context.utxos(script_address)
+                            if u.output.amount.multi_asset.get(script_hash).get(
+                                AssetName(b"lord tuna")
+                            )
+                        ][0]
+
+                        validator_state = FortunaParams.from_cbor(
+                            validator_out_ref.output.datum.cbor
+                        )
+                        assert validator_state is not None, "Datum is missing"
+                        target_state = FortunaState(
+                            nonce=nonce.to_bytes(16, "big"),
+                            block_number=validator_state.block_number,
+                            current_hash=validator_state.current_hash,
+                            leading_zeroes=validator_state.leading_zeroes,
+                            difficulty=validator_state.difficulty,
+                            epoch_time=validator_state.epoch_time,
+                        )
+                target_state.nonce = nonce.to_bytes(16, "big")
+                target_hash = sha256(sha256(target_state.to_cbor()).digest()).digest()
+                leading_zeroes, difficulty = get_difficulty(target_hash)
+                if leading_zeroes > target_state.leading_zeroes or (
+                    leading_zeroes == target_state.leading_zeroes
+                    and difficulty < target_state.difficulty
+                ):
+                    break
+                nonce += 1
+
+            print(f"Found block with nonce {nonce}")
+            realtimenow = int(time.time()) * 1000 - 60000
+            interlink = calculate_interlink(
+                target_hash,
+                (leading_zeroes, difficulty),
+                (validator_state.leading_zeroes, validator_state.difficulty),
+                validator_state.interlink,
+            )
+
+            epoch_time = (
+                (validator_state.difficulty)
+                + (realtimenow + 90000)
+                - validator_state.epoch_time
+            )
+            difficulty_number = validator_state.difficulty
+            leading_zeroes = validator_state.leading_zeroes
+            if validator_state.block_number % 2016 == 0 and validator_state.block_number > 0:
+                adjustment = get_difficulty_adjustment(epoch_time, 1_209_600_000)
+                epoch_time = 0
+                difficulty_number, leading_zeroes = calculate_difficulty(
+                    (validator_state.leading_zeroes, validator_state.difficulty),
+                    adjustment,
                 )
-            ][0]
 
-            if validator_out_ref_new != validator_out_ref:
-                validator_out_ref = [
-                    u
-                    for u in context.utxos(script_address)
-                    if u.output.amount.multi_asset.get(script_hash).get(
-                        AssetName(b"lord tuna")
-                    )
-                ][0]
+            post_datum = FortunaParams(
+                block_number=validator_state.block_number + 1,
+                current_hash=target_hash,
+                leading_zeroes=leading_zeroes,
+                difficulty=difficulty_number,
+                epoch_time=epoch_time,
+                real_time_now=90_000_000 + realtimenow,
+                message=b"AlL HaIl tUnA",
+                interlink=interlink,
+            )
 
-                validator_state = FortunaParams.from_cbor(
-                    validator_out_ref.output.datum.cbor
-                )
-                assert validator_state is not None, "Datum is missing"
-                target_state = FortunaState(
-                    nonce=nonce.to_bytes(16, "big"),
-                    block_number=validator_state.block_number,
-                    current_hash=validator_state.current_hash,
-                    leading_zeroes=validator_state.leading_zeroes,
-                    difficulty=validator_state.difficulty,
-                    epoch_time=validator_state.epoch_time,
-                )
-        target_state.nonce = nonce.to_bytes(16, "big")
-        target_hash = sha256(sha256(target_state.to_cbor()).digest()).digest()
-        leading_zeroes, difficulty = get_difficulty(target_hash)
-        if leading_zeroes > target_state.leading_zeroes or (
-            leading_zeroes == target_state.leading_zeroes
-            and difficulty < target_state.difficulty
-        ):
-            break
-        nonce += 1
+            # Make redeemer
+            redeemer = Redeemer(FortunaRedeemer(nonce.to_bytes(16, "big")))
 
-    print(f"Found block with nonce {nonce}")
-    realtimenow = int(time.time()) * 1000 - 60000
-    interlink = calculate_interlink(
-        target_hash,
-        (leading_zeroes, difficulty),
-        (validator_state.leading_zeroes, validator_state.difficulty),
-        validator_state.interlink,
-    )
+            # Build the transaction
+            builder = TransactionBuilder(context)
+            builder.add_script_input(validator_out_ref, script=script_utxo, redeemer=redeemer)
+            # we must specify at least the start of the tx valid range in slots
+            builder.validity_start = realtimenow
+            # This specifies the end of tx valid range in slots
+            builder.ttl = builder.validity_start + 1000
+            builder.add_minting_script(script_utxo, Redeemer(PlutusData()))
+            builder.mint = MultiAsset({script_hash: {AssetName(b"TUNA"): 5_000_000_000}})
+            new_output = copy(validator_out_ref.output)
+            new_output.datum = post_datum
+            builder.add_output(new_output)
 
-    epoch_time = (
-        (validator_state.difficulty)
-        + (realtimenow + 90000)
-        - validator_state.epoch_time
-    )
-    difficulty_number = validator_state.difficulty
-    leading_zeroes = validator_state.leading_zeroes
-    if validator_state.block_number % 2016 == 0 and validator_state.block_number > 0:
-        adjustment = get_difficulty_adjustment(epoch_time, 1_209_600_000)
-        epoch_time = 0
-        difficulty_number, leading_zeroes = calculate_difficulty(
-            (validator_state.leading_zeroes, validator_state.difficulty),
-            adjustment,
-        )
+            # Sign the transaction
+            payment_vkey, payment_skey, _ = get_signing_info("miner")
+            signed_tx = builder.build_and_sign(
+                signing_keys=[payment_skey],
+                change_address=payment_address,
+            )
 
-    post_datum = FortunaParams(
-        block_number=validator_state.block_number + 1,
-        current_hash=target_hash,
-        leading_zeroes=leading_zeroes,
-        difficulty=difficulty_number,
-        epoch_time=epoch_time,
-        real_time_now=90_000_000 + realtimenow,
-        message=b"AlL HaIl tUnA",
-        interlink=interlink,
-    )
+            # Submit the transaction
+            context.submit_tx(signed_tx.to_cbor())
 
-    # Make redeemer
-    redeemer = Redeemer(FortunaRedeemer(nonce.to_bytes(16, "big")))
+            # context.submit_tx(signed_tx.to_cbor())
+            print(f"transaction id: {signed_tx.id}")
+            if network == Network.TESTNET:
+                print(f"Cexplorer: https://preview.cexplorer.io/tx/{signed_tx.id}")
+            else:
+                print(f"Cexplorer: https://cexplorer.io/tx/{signed_tx.id}")
+        except Exception as e:
+            print("Unexpected error")
+            print(e)
+            time.sleep(5)
 
-    # Build the transaction
-    builder = TransactionBuilder(context)
-    builder.add_script_input(validator_out_ref, script=script_utxo, redeemer=redeemer)
-    # we must specify at least the start of the tx valid range in slots
-    builder.validity_start = realtimenow
-    # This specifies the end of tx valid range in slots
-    builder.ttl = builder.validity_start + 1000
-    builder.add_minting_script(script_utxo, Redeemer(PlutusData()))
-    builder.mint = MultiAsset({script_hash: {AssetName(b"TUNA"): 5_000_000_000}})
-    new_output = copy(validator_out_ref.output)
-    new_output.datum = post_datum
-    builder.add_output(new_output)
 
-    # Sign the transaction
-    payment_vkey, payment_skey, _ = get_signing_info("miner")
-    signed_tx = builder.build_and_sign(
-        signing_keys=[payment_skey],
-        change_address=payment_address,
-    )
-
-    # Submit the transaction
-    context.submit_tx(signed_tx.to_cbor())
-
-    # context.submit_tx(signed_tx.to_cbor())
-    print(f"transaction id: {signed_tx.id}")
-    if network == Network.TESTNET:
-        print(f"Cexplorer: https://preview.cexplorer.io/tx/{signed_tx.id}")
-    else:
-        print(f"Cexplorer: https://cexplorer.io/tx/{signed_tx.id}")
 
 
 if __name__ == "__main__":
